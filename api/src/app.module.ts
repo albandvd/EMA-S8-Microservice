ba346b7353b591842078796df295fa806d424bb6
Alban DAVID <alban.david06@gmail.com>
2026-06-22 15:31:17 +0200

    feat: init flights

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AirlinesModule } from './airlines/airlines.module';
+import { FlightsModule } from './flights/flights.module';
 
 @Module({
-  imports: [AirlinesModule],
+  imports: [AirlinesModule, FlightsModule],
   controllers: [AppController],
   providers: [AppService],
 })
